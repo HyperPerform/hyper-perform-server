05a3ff7e84a597a9dadb665a9c4bbfab7cdcc143
rohanchhipa <rohan.chhipa@live.com>
2016-08-19 14:22:34 +0200

    Added concrete implementation of algorithm

---
+++ src/main/java/me/hyperperform/reporting/algorithm/StandardAlgorithm.java
@@ -0,0 +1,15 @@
+package me.hyperperform.reporting.algorithm;
+
+import me.hyperperform.reporting.request.CalculateScoreRequest;
+import me.hyperperform.reporting.response.CalculateScoreResponse;
+
+/**
+ * Created by rohan on 2016/08/19.
+ */
+public class StandardAlgorithm implements Algorithm
+{
+    public CalculateScoreResponse calculateScore(CalculateScoreRequest calculateScoreRequest)
+    {
+        return null;
+    }
+}
