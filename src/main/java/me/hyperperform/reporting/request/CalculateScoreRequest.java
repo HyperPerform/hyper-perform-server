5acd9a4bde8ae54d80e5f186ae3c31f98143410e
rohanchhipa <rohan.chhipa@live.com>
2016-08-22 12:44:00 +0200

    Added fields for request

--- src/main/java/me/hyperperform/reporting/request/CalculateScoreRequest.java
+++ src/main/java/me/hyperperform/reporting/request/CalculateScoreRequest.java
@@ -1,7 +1,37 @@
 package me.hyperperform.reporting.request;
 
+import java.sql.Timestamp;
+
 /**
  * Created by rohan on 2016/08/19.
  */
 public class CalculateScoreRequest {
+
+    private String name;
+    private Timestamp startDate;
+    private Timestamp endDate;
+
+    public Timestamp getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Timestamp endDate) {
+        this.endDate = endDate;
+    }
+
+    public Timestamp getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Timestamp startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
