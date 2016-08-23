799cea4c8b4d14e71e7a7328e296bb76a095fc15
rohanchhipa <rohan.chhipa@live.com>
2016-08-23 16:25:10 +0200

    Added contracts

--- src/main/java/me/hyperperform/reporting/IReport.java
+++ src/main/java/me/hyperperform/reporting/IReport.java
@@ -1,7 +1,13 @@
 package me.hyperperform.reporting;
 
+import me.hyperperform.reporting.request.*;
+import me.hyperperform.reporting.response.*;
+
 /**
  * Created by rohan on 2016/08/18.
  */
 public interface IReport {
+    GetSummaryResponse getSummary(GetSummaryRequest getSummaryRequest);
+    GetDetailsResponse getDetails(GetDetailsRequest getDetailsRequest);
+    GetScoreResponse getScore(GetScoreRequest getScoreRequest);
 }
