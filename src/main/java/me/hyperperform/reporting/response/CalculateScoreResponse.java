ad5911b8e6aa296ff94d40d394ccea51cee690c7
rohanchhipa <rohan.chhipa@live.com>
2016-08-23 09:54:32 +0200

    Added fields for score

--- src/main/java/me/hyperperform/reporting/response/CalculateScoreResponse.java
+++ src/main/java/me/hyperperform/reporting/response/CalculateScoreResponse.java
@@ -4,4 +4,18 @@ package me.hyperperform.reporting.response;
  * Created by rohan on 2016/08/19.
  */
 public class CalculateScoreResponse {
+
+    private double score;
+
+    public CalculateScoreResponse(double score) {
+        this.score = score;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
 }
