5ebba4e636db691130030e343367b3c0e50db506
rohanchhipa <rohan.chhipa@live.com>
2016-08-23 16:24:06 +0200

    Added getDetails request object

---
+++ src/main/java/me/hyperperform/reporting/request/GetDetailsRequest.java
@@ -0,0 +1,46 @@
+package me.hyperperform.reporting.request;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by rohan on 2016/08/23.
+ */
+public class GetDetailsRequest {
+
+    private String name;
+    private Timestamp startDate;
+    private Timestamp endDate;
+    private String type;
+
+    public Timestamp getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = Timestamp.valueOf(endDate);
+    }
+
+    public Timestamp getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = Timestamp.valueOf(startDate);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
