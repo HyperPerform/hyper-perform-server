1f7bc18a000095db70bbf180143003aedbe5d5d5
rohanchhipa <rohan.chhipa@live.com>
2016-09-01 16:14:18 +0200

    Added javadoc for Algorithm interface

--- src/main/java/me/hyperperform/reporting/algorithm/Algorithm.java
+++ src/main/java/me/hyperperform/reporting/algorithm/Algorithm.java
@@ -4,10 +4,20 @@ import me.hyperperform.reporting.request.CalculateScoreRequest;
 import me.hyperperform.reporting.response.CalculateScoreResponse;
 
 /**
- * Created by rohan on 2016/08/19.
+ * Hyper-perform
+ * Group: CodusMaximus
+ * Date: 2016/08/19
+ * Feature: Algorithm Strategy
+ *
+ * Algorithm interface for Strategy design pattern. The Algorithms are used to calculate performance scores.
  */
 public interface Algorithm {
 
+    /**
+     * A method that all Concrete Strategies must implement.
+     * @param calculateScoreRequest Contains the necessary parameters for score calculations.
+     * @return Response object which contains the score.
+     */
     CalculateScoreResponse calculateScore(CalculateScoreRequest calculateScoreRequest);
 
 }
