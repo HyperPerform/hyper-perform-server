5c995d1ce769cc56a92dcceb282608aaad870e2a
rohanchhipa <rohan.chhipa@live.com>
2016-09-01 16:19:39 +0200

    Added reporting interface javadoc

--- src/main/java/me/hyperperform/reporting/IReport.java
+++ src/main/java/me/hyperperform/reporting/IReport.java
@@ -4,10 +4,25 @@ import me.hyperperform.reporting.request.*;
 import me.hyperperform.reporting.response.*;
 
 /**
+ * Serves as the Context in strategy design pattern. Each report generator is has an Algorithm.
+ *
  * Created by rohan on 2016/08/18.
  */
 public interface IReport {
+
+    /**
+     * Calculates and returns summary details
+     */
     GetSummaryResponse getSummary(GetSummaryRequest getSummaryRequest);
+
+    /**
+     * Returns more fine grained details for a specified integration. The integration is specified in the
+     * request object
+     */
     GetDetailsResponse getDetails(GetDetailsRequest getDetailsRequest);
+
+    /**
+     * Calculates performace score using an algorithm
+     */
     GetScoreResponse getScore(GetScoreRequest getScoreRequest);
 }
