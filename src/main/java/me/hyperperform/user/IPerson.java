904825ab48e8711053fefe9aa9060c8de515bfa5
Avinash Singh <u14043778@tuks.co.za>
2016-09-06 09:59:48 +0200

    added interface contract for users

---
+++ src/main/java/me/hyperperform/user/IPerson.java
@@ -0,0 +1,34 @@
+package me.hyperperform.user;
+
+
+
+/**
+ * hyperperform-system
+ * Group: CodusMaximus
+ * Date: 2016/09/05
+ * Feature:
+ */
+public interface IPerson
+{
+    String getUserName();
+
+    String getUserSurname();
+
+    String getUserEmail();
+
+    String getUserPassword();
+
+    String getUserID();
+
+
+
+    void setUserName(String userName);
+
+    void setUserSurname(String userSurname);
+
+    void setUserEmail(String userEmail);
+
+    void setUserID(String userID);
+
+    void setUserPassword(String userPassword);
+}
