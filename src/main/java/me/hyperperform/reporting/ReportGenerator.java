f68b39a2343010b64fdd6981411e55ed7887ac9f
rohanchhipa <rohan.chhipa@live.com>
2016-09-06 18:18:33 +0200

    now creates graph data for front end devices

--- src/main/java/me/hyperperform/reporting/ReportGenerator.java
+++ src/main/java/me/hyperperform/reporting/ReportGenerator.java
@@ -11,6 +11,7 @@ import me.hyperperform.reporting.response.*;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.persistence.*;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -138,7 +139,44 @@ public class ReportGenerator implements IReport
                 data.get(repos.indexOf(curr.getRepository())).add(curr);
             }
 
-            getDetailsResponse.setGitDetails(new GitDetails(data.size(), data));
+
+
+            long range = (getDetailsRequest.getEndDate().getTime() - getDetailsRequest.getStartDate().getTime());
+            range /= 10;
+
+            ArrayList<GraphData<String, Long>> graphData = new ArrayList<GraphData<String, Long>>();
+
+            for (int k = 0; k < repos.size(); k++)
+            {
+                ArrayList<String> xAxis = new ArrayList<String>();
+                ArrayList<Long> yAxis = new ArrayList<Long>();
+
+                long prev = getDetailsRequest.getStartDate().getTime();
+                for (int j = 1; j <= 10; j++)
+                {
+                    long curr = prev + range;
+
+                    Query dataQuery = entityManager.createQuery("SELECT SUM(a.commitSize) FROM GitPush a WHERE (timestamp BETWEEN :startDate AND :endDate) AND (username=:uname) AND (repository=:repo)")
+                            .setParameter("startDate", new Timestamp(prev))
+                            .setParameter("endDate", new Timestamp(curr))
+                            .setParameter("uname", getDetailsRequest.getName())
+                            .setParameter("repo", repos.get(k));
+
+                    Object commits = dataQuery.getSingleResult();
+
+                    String currXLabel = (new Timestamp(curr)).toString();
+
+                    xAxis.add(currXLabel.substring(0, currXLabel.indexOf(" ")));
+                    yAxis.add((commits == null) ? 0 : (Long) commits);
+
+                    prev = curr;
+                }
+
+                graphData.add(new GraphData<String, Long>(xAxis, yAxis));
+            }
+
+            getDetailsResponse.setGitDetails(new GitDetails(data.size(), data, graphData));
+
         }
         else
         if (getDetailsRequest.getType().equals("issues"))
