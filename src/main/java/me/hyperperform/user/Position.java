96b62e9264a0322de3e42b9d05a9ebb5bca435dd
Avinash Singh <u14043778@tuks.co.za>
2016-09-09 09:59:38 +0200

    added Enum for position and roles

---
+++ src/main/java/me/hyperperform/user/Position.java
@@ -0,0 +1,14 @@
+package me.hyperperform.user;
+
+/**
+ * hyperperform-system
+ * Group: CodusMaximus
+ * Date: 2016/09/09
+ * Feature:
+ */
+public enum Position
+{
+    WebDeveloper,
+    SoftwareDeveloper,
+    Multimedia
+}
