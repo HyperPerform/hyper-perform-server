81c6828d567af9e0bb0ff581e9dba0380fc58b92
Avinash Singh <u14043778@tuks.co.za>
2016-09-09 09:28:17 +0200

    added initial Notifications module structure

---
+++ src/main/java/me/hyperperform/notification/INotification.java
@@ -0,0 +1,14 @@
+package me.hyperperform.notification;
+
+/**
+ * hyperperform-system
+ * Group: CodusMaximus
+ * Date: 2016/09/09
+ * Feature:
+ */
+public interface INotification
+{
+    void newUserNotification();
+    void changePasswordNotification();
+
+}
---
+++ src/main/java/me/hyperperform/notification/Notifications.java
@@ -0,0 +1,21 @@
+package me.hyperperform.notification;
+
+/**
+ * hyperperform-system
+ * Group: CodusMaximus
+ * Date: 2016/09/09
+ * Feature:
+ */
+public class Notifications implements INotification
+{
+
+    public void newUserNotification()
+    {
+
+    }
+
+    public void changePasswordNotification()
+    {
+
+    }
+}
