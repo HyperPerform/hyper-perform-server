3a8acb4fdba94320b83a8001d198662aab760ede
rohanchhipa <rohan.chhipa@live.com>
2016-09-21 17:03:53 +0200

    Added mock users

---
+++ src/test/java/me/hyperperform/user/MockUsers.java
@@ -0,0 +1,9 @@
+package me.hyperperform.user;
+
+/**
+ * Created by rohan on 2016/09/21.
+ */
+public class MockUsers
+{
+    static String normalUser = "{userName:\"rohan\", userSurname:\"chhipa\", userEmail:\"rohan.chhipa@live.com\", userPassword:\"1234\", role:\"Employee\", position:\"SoftwareDeveloper\", }";
+}
