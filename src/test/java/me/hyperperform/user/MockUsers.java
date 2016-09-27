7e8c667996e1afec1e28d4f1a9ca843238e2c4c6
rohanchhipa <rohan.chhipa@live.com>
2016-09-27 09:49:10 +0200

    changed mock data to remove email clash in testing

--- src/test/java/me/hyperperform/user/MockUsers.java
+++ src/test/java/me/hyperperform/user/MockUsers.java
@@ -5,16 +5,16 @@ package me.hyperperform.user;
  */
 public class MockUsers
 {
-    static String normalUser = "{\"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohan.chhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\"}";
+    static String normalUser = "{\"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\"}";
 
-    static String noUsername = "{\"userName\":\"\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohan.chhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\"}";
+    static String noUsername = "{\"userName\":\"\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\"}";
 
-    static String noSurname = "{\"userName\":\"rohan\", \"userSurname\":\"\", \"userEmail\":\"rohan.chhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\"}";
+    static String noSurname = "{\"userName\":\"rohan\", \"userSurname\":\"\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\"}";
 
     static String noEmail = "{\"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\"}";
 
-    static String invalidRole = "{\"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohan.chhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Jester\", \"position\":\"SoftwareDeveloper\"}";
+    static String invalidRole = "{\"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Jester\", \"position\":\"SoftwareDeveloper\"}";
 
-    static String invalidPosition = "{\"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohan.chhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"Outside\"}";
+    static String invalidPosition = "{\"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"Outside\"}";
 
 }
