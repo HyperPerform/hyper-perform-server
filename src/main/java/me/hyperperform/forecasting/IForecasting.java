298c427d1cdb80e3dc9e54f91cf170e40ebf3c7d
rohanchhipa <rohan.chhipa@live.com>
2016-10-02 08:26:16 +0200

    Added add integration contract

--- src/main/java/me/hyperperform/forecasting/IForecasting.java
+++ src/main/java/me/hyperperform/forecasting/IForecasting.java
@@ -1,7 +1,11 @@
 package me.hyperperform.forecasting;
 
+import me.hyperperform.forecasting.request.AddIntegrationRequest;
 import me.hyperperform.forecasting.request.GetIntegrationsRequest;
+import me.hyperperform.forecasting.request.UpdateIntegrationRequest;
+import me.hyperperform.forecasting.response.AddIntegrationResponse;
 import me.hyperperform.forecasting.response.GetIntegrationsResponse;
+import me.hyperperform.forecasting.response.UpdateIntegrationResponse;
 
 /**
  * Created by rohan on 2016/10/02.
@@ -9,4 +13,6 @@ import me.hyperperform.forecasting.response.GetIntegrationsResponse;
 public interface IForecasting
 {
     GetIntegrationsResponse getIntegrations(GetIntegrationsRequest getIntegrationsRequest);
+    UpdateIntegrationResponse updateIntegration(UpdateIntegrationRequest updateIntegrationRequest);
+    AddIntegrationResponse addIntegration(AddIntegrationRequest addIntegrationRequest);
 }
