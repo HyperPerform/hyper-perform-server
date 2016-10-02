44d71662584306b618c963429a5d412266f871d6
rohanchhipa <rohan.chhipa@live.com>
2016-10-02 08:30:31 +0200

    Added realization of IForecasting

---
+++ src/main/java/me/hyperperform/forecasting/Forecasting.java
@@ -0,0 +1,27 @@
+package me.hyperperform.forecasting;
+
+import me.hyperperform.forecasting.request.AddIntegrationRequest;
+import me.hyperperform.forecasting.request.GetIntegrationsRequest;
+import me.hyperperform.forecasting.request.UpdateIntegrationRequest;
+import me.hyperperform.forecasting.response.AddIntegrationResponse;
+import me.hyperperform.forecasting.response.GetIntegrationsResponse;
+import me.hyperperform.forecasting.response.UpdateIntegrationResponse;
+
+/**
+ * Created by rohan on 2016/10/02.
+ */
+public class Forecasting implements IForecasting
+{
+
+    public GetIntegrationsResponse getIntegrations(GetIntegrationsRequest getIntegrationsRequest) {
+        return null;
+    }
+
+    public UpdateIntegrationResponse updateIntegration(UpdateIntegrationRequest updateIntegrationRequest) {
+        return null;
+    }
+
+    public AddIntegrationResponse addIntegration(AddIntegrationRequest addIntegrationRequest) {
+        return null;
+    }
+}
