0396bb9fba7ff9ec3e9519205cc32b7e28def08e
rohanchhipa <rohan.chhipa@live.com>
2016-10-02 11:48:55 +0200

    Added constructor

--- src/main/java/me/hyperperform/forecasting/ForecastData.java
+++ src/main/java/me/hyperperform/forecasting/ForecastData.java
@@ -1,11 +1,25 @@
 package me.hyperperform.forecasting;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by rohan on 2016/10/02.
  */
+
+@Entity
+@Table(name = "\"ForecastData\"")
 public class ForecastData
 {
+    @Id
+    @Column(name = "data", length = 10485760)
     private String data;
+
+    public ForecastData(String data) {
+        this.data = data;
+    }
 
     public String getData() {
         return data;
