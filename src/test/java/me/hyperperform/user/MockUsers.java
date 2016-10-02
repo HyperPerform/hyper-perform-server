e9fa394ce1c8762a84edf9834227f3173ee223c6
Avinash Singh <u14043778@tuks.co.za>
2016-10-02 15:07:02 +0200

    added git username to the mock users

--- src/test/java/me/hyperperform/user/MockUsers.java
+++ src/test/java/me/hyperperform/user/MockUsers.java
@@ -2,16 +2,16 @@ package me.hyperperform.user;
 
 public class MockUsers
 {
-    static String normalUser = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\"}";
+    static String normalUser = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\", \"gitUserName\": \"rohan\"}";
 
-    static String noUsername = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\"}";
+    static String noUsername = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\", \"gitUserName\": \"rohan\"}";
 
-    static String noSurname = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"rohan\", \"userSurname\":\"\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\"}";
+    static String noSurname = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"rohan\", \"userSurname\":\"\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\", \"gitUserName\": \"rohan\"}";
 
-    static String noEmail = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\"}";
+    static String noEmail = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\", \"gitUserName\": \"rohan\"}";
 
-    static String invalidRole = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Jester\", \"position\":\"SoftwareDeveloper\"}";
+    static String invalidRole = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Jester\", \"position\":\"SoftwareDeveloper\", \"gitUserName\": \"rohan\"}";
 
-    static String invalidPosition = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"Outside\"}";
+    static String invalidPosition = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"Outside\", \"gitUserName\": \"rohan\"}";
 
 }
