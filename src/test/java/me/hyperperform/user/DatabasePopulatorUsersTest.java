11938ca4c78c9b24c387afc58b462c104d12633e
AvinashSingh786 <u14043778@tuks.co.za>
2016-10-02 19:34:30 +0200

    now uses actual git usernames

--- src/test/java/me/hyperperform/user/DatabasePopulatorUsersTest.java
+++ src/test/java/me/hyperperform/user/DatabasePopulatorUsersTest.java
@@ -37,9 +37,9 @@ public class DatabasePopulatorUsersTest
         System.out.println("Adding mock users to database...");
         ArrayList<User> users = new ArrayList<User>();
         users.add(new User("admin@hyperperform.me", "Admin", "admin", "admin", "admin", "1234", null, EmployeeRole.Administrator, Position.Manager));
-        users.add(new User("rohan.chhipa@live.com", "Rohan", "rohanchhipa", "Rohan", "Chhipa", "1234", null, EmployeeRole.Employee, Position.SoftwareDeveloper));
-        users.add(new User("avinash.singh@gmail.com", "Avinash", "avinashsingh786", "Avinash", "Singh", "1234", null, EmployeeRole.Employee, Position.SoftwareDeveloper));
-        users.add(new User("jason.gordon@gmail.com", "Jason", "jasongordon", "Jason", "Gordon", "1234", null, EmployeeRole.Employee, Position.SoftwareDeveloper));
+        users.add(new User("rohan.chhipa@live.com", "Rohan", "RohanChhipa", "Rohan", "Chhipa", "1234", null, EmployeeRole.Employee, Position.SoftwareDeveloper));
+        users.add(new User("avinash.singh@gmail.com", "Avinash", "AvinashSingh786", "Avinash", "Singh", "1234", null, EmployeeRole.Employee, Position.SoftwareDeveloper));
+        users.add(new User("jason.gordon@gmail.com", "Jason", "JasonDGordon", "Jason", "Gordon", "1234", null, EmployeeRole.Employee, Position.SoftwareDeveloper));
         users.add(new User("claudio.da.silva@live.com", "Claudio", "claudiodasilva", "Claudio", "da Silva", "1234", null, EmployeeRole.Employee, Position.Multimedia));
 
         entityTransaction.begin();
