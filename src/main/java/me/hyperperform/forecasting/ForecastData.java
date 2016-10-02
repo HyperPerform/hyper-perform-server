068c39c03eeecb8f06958e444e3220b014ba5c04
rohanchhipa <rohan.chhipa@live.com>
2016-10-02 10:49:33 +0200

    added persistable pojo for forecast data

---
+++ src/main/java/me/hyperperform/forecasting/ForecastData.java
@@ -0,0 +1,17 @@
+package me.hyperperform.forecasting;
+
+/**
+ * Created by rohan on 2016/10/02.
+ */
+public class ForecastData
+{
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
