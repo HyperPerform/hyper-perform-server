c46b4c73eb54785d01a8193a20735f987b08f95d
rohanchhipa <rohan.chhipa@live.com>
2016-10-02 11:59:05 +0200

    Added data placeholder along with getters and setters

--- src/main/java/me/hyperperform/forecasting/response/GetIntegrationsResponse.java
+++ src/main/java/me/hyperperform/forecasting/response/GetIntegrationsResponse.java
@@ -3,5 +3,15 @@ package me.hyperperform.forecasting.response;
 /**
  * Created by rohan on 2016/10/02.
  */
-public class GetIntegrationsResponse {
+public class GetIntegrationsResponse
+{
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
 }
