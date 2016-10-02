d3603c994f13fae102d732408b0c6e076d7de617
rohanchhipa <rohan.chhipa@live.com>
2016-10-02 12:56:46 +0200

    Added new deleteIntegration method

--- src/main/java/me/hyperperform/forecasting/IForecasting.java
+++ src/main/java/me/hyperperform/forecasting/IForecasting.java
@@ -1,9 +1,11 @@
 package me.hyperperform.forecasting;
 
 import me.hyperperform.forecasting.request.AddIntegrationRequest;
+import me.hyperperform.forecasting.request.DeleteIntegrationRequest;
 import me.hyperperform.forecasting.request.GetIntegrationsRequest;
 import me.hyperperform.forecasting.request.UpdateIntegrationRequest;
 import me.hyperperform.forecasting.response.AddIntegrationResponse;
+import me.hyperperform.forecasting.response.DeleteIntegrationResponse;
 import me.hyperperform.forecasting.response.GetIntegrationsResponse;
 import me.hyperperform.forecasting.response.UpdateIntegrationResponse;
 
@@ -15,4 +17,5 @@ public interface IForecasting
     GetIntegrationsResponse getIntegrations(GetIntegrationsRequest getIntegrationsRequest);
     UpdateIntegrationResponse updateIntegration(UpdateIntegrationRequest updateIntegrationRequest);
     AddIntegrationResponse addIntegration(AddIntegrationRequest addIntegrationRequest);
+    DeleteIntegrationResponse deleteIntegration(DeleteIntegrationRequest deleteIntegrationRequest);
 }
