47b7a1bf31d5008c190ab280c1f4f36329fe73a6
rohanchhipa <rohan.chhipa@live.com>
2016-10-02 08:11:11 +0200

    Added getIntegrations method

--- src/main/java/me/hyperperform/forecasting/IForecasting.java
+++ src/main/java/me/hyperperform/forecasting/IForecasting.java
@@ -1,9 +1,12 @@
 package me.hyperperform.forecasting;
 
+import me.hyperperform.forecasting.request.GetIntegrationsRequest;
+import me.hyperperform.forecasting.response.GetIntegrationsResponse;
+
 /**
  * Created by rohan on 2016/10/02.
  */
 public interface IForecasting
 {
-
+    GetIntegrationsResponse getIntegrations(GetIntegrationsRequest getIntegrationsRequest);
 }
