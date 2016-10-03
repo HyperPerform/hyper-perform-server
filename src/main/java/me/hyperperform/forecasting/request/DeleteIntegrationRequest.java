8371c664284c0971defdcf1fb22e1a714199ae18
rohanchhipa <rohan.chhipa@live.com>
2016-10-03 10:32:08 +0200

    Added field for name along with getters and setters

--- src/main/java/me/hyperperform/forecasting/request/DeleteIntegrationRequest.java
+++ src/main/java/me/hyperperform/forecasting/request/DeleteIntegrationRequest.java
@@ -3,5 +3,19 @@ package me.hyperperform.forecasting.request;
 /**
  * Created by rohan on 2016/10/02.
  */
-public class DeleteIntegrationRequest {
+public class DeleteIntegrationRequest
+{
+    private String integrationName;
+
+    public DeleteIntegrationRequest(String integrationName) {
+        this.integrationName = integrationName;
+    }
+
+    public String getIntegrationName() {
+        return integrationName;
+    }
+
+    public void setIntegrationName(String integrationName) {
+        this.integrationName = integrationName;
+    }
 }
