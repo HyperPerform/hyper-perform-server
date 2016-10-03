f09fdc545121cc36308862284854254dfe60248e
rohanchhipa <rohan.chhipa@live.com>
2016-10-03 12:08:37 +0200

    Added constructor and setter

--- src/main/java/me/hyperperform/forecasting/request/UpdateIntegrationRequest.java
+++ src/main/java/me/hyperperform/forecasting/request/UpdateIntegrationRequest.java
@@ -4,8 +4,19 @@ public class UpdateIntegrationRequest
 {
     private String data;
 
+    public UpdateIntegrationRequest() {
+    }
+
+    public UpdateIntegrationRequest(String data) {
+        this.data = data;
+    }
+
     public String getData()
     {
         return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
     }
 }
