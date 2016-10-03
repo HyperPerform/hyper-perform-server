04a15d5ced0b86b87eaf16ea6282c14f4602c344
rohanchhipa <rohan.chhipa@live.com>
2016-10-03 10:32:30 +0200

    Added boolean field to show if integration was deleted

--- src/main/java/me/hyperperform/forecasting/response/DeleteIntegrationResponse.java
+++ src/main/java/me/hyperperform/forecasting/response/DeleteIntegrationResponse.java
@@ -3,5 +3,18 @@ package me.hyperperform.forecasting.response;
 /**
  * Created by rohan on 2016/10/02.
  */
-public class DeleteIntegrationResponse {
+public class DeleteIntegrationResponse
+{
+    private boolean deleted;
+
+    public DeleteIntegrationResponse() {
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
 }
