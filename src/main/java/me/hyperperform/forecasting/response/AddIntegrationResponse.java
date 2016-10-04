a8e0cffe4bea79f4fa56b90821b7b7796e0ba97b
RohanChhipa <rohan.chhipa@live.com>
2016-10-04 07:26:52 +0200

    Added boolean added field along with getters and setters

--- src/main/java/me/hyperperform/forecasting/response/AddIntegrationResponse.java
+++ src/main/java/me/hyperperform/forecasting/response/AddIntegrationResponse.java
@@ -3,5 +3,15 @@ package me.hyperperform.forecasting.response;
 /**
  * Created by rohan on 2016/10/02.
  */
-public class AddIntegrationResponse {
+public class AddIntegrationResponse
+{
+    private boolean added;
+
+    public boolean isAdded() {
+        return added;
+    }
+
+    public void setAdded(boolean added) {
+        this.added = added;
+    }
 }
