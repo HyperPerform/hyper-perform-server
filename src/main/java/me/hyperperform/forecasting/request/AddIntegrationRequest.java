5214098267486ad47d5791cfd32729a856ee775e
RohanChhipa <rohan.chhipa@live.com>
2016-10-04 07:29:39 +0200

    Added data field along with getters and setters

--- src/main/java/me/hyperperform/forecasting/request/AddIntegrationRequest.java
+++ src/main/java/me/hyperperform/forecasting/request/AddIntegrationRequest.java
@@ -3,5 +3,15 @@ package me.hyperperform.forecasting.request;
 /**
  * Created by rohan on 2016/10/02.
  */
-public class AddIntegrationRequest {
+public class AddIntegrationRequest
+{
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
 }
