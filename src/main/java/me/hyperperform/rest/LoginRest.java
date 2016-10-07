4b8214ba0020aff2cfaaea9b32cacff68b0dd26d
AvinashSingh786 <u14043778@tuks.co.za>
2016-10-07 22:02:07 +0200

    few tweaks and now sets the new random users password

--- src/main/java/me/hyperperform/rest/LoginRest.java
+++ src/main/java/me/hyperperform/rest/LoginRest.java
@@ -331,17 +331,19 @@ public class LoginRest
         entityManagerFactory = Persistence.createEntityManagerFactory("PostgreJPA");
         entityManager = entityManagerFactory.createEntityManager();
         entityTransaction = entityManager.getTransaction();
-
+        System.out.println("\n\n\n"+forgot.getEmail());
         Query q = entityManager.createQuery("SELECT u FROM User u WHERE userEmail=:email").setParameter("email", forgot.getEmail());
         List<User> list = q.getResultList();
 
         ForgotPasswordResponse res = null;
         String pass = Hash.randPass(6);
+        System.out.println("\n\n Size: " + list.size() );
         if (list.size() == 1)
         {
+            System.out.println("\n\n Update");
             q = entityManager.createQuery("UPDATE User SET userPassword=:pass WHERE userEmail=:email")
                     .setParameter("email", forgot.getEmail())
-                    .setParameter( "pass", pass);
+                    .setParameter( "pass", Hash.gethash(pass));
 
             entityTransaction.begin();
             q.executeUpdate();
@@ -350,9 +352,51 @@ public class LoginRest
             if (mail == null )
              mail = new Email();
 
-            String body = "Some test : " + pass;
+            String body = "<div>" +
+                    "<div class=\"center\">" +
+                    "<table cellpadding=\"0\" cellspacing=\"0\" width=\"682px\">" +
+                    "<tbody>" +
+                    "<tr>" +
+                    "<td style=\"background-color: rgba(0,0,0, 0.85);\">" +
+                    "<div style=\"margin-left:28%;\">" +
+                    "<img src=\"https://dashboard.hyperperform.me/assets/hyperperform_text.png\" height=\"100px\" alt=\"Logo\" style=\"display: inline; alt=\"HyperPerform\" margin-left: -1.5%;\">" +
+                    "</div>" +
+                    "<br/><br/>" +
+                    "</td>" +
+                    "</tr>" +
+                    "<tr style=\"position: absolute; margin-top: -2%\">" +
+                    "<td valign=\"top\">" +
+                    "<table cellpadding=\"0\" cellspacing=\"0\" width=\"682px\" style=\"width:682px;height:263px;background-color:#fff;border-left:solid 1px #999894;border-right:solid 1px #999894\">" +
+                    "<tbody>" +
+                    "<tr>" +
+                    "<td valign=\"top\" align=\"left\">" +
+                    "<table cellpadding=\"25\" width=\"90%\">" +
+                    "<tbody>" +
+                    "<tr>" +
+                    "<td>" +
+                    "<h3> Dear " + list.get(0).getName() + " </h3>" +
+                    "<p> You have requested a new password for our HyperPerform system please find your details below to login:</p>" +
+                    "<p>Email: " + list.get(0).getUserEmail() + "</p>" +
+                    "<p>Password: " + pass + "</p>" +
+                    "<br/><br/><br/>" +
+                    "Kind Regards<br/>" +
+                    "<b>HyperPerform Team</b><br/>" +
+                    "</td>" +
+                    "</tr>" +
+                    "</tbody>" +
+                    "</table>" +
+                    "</td>" +
+                    "</tr>" +
+                    "</tbody>" +
+                    "</table>" +
+                    "</td>" +
+                    "</tr>" +
+                    "</tbody>" +
+                    "</table>" +
+                    "</div>" +
+                    "</div>";
             res = new ForgotPasswordResponse("Success, you will shortly receive an email");
-            mail.sendMail(forgot.getEmail(), "Hello there ", body);
+            mail.sendMail(forgot.getEmail(), "Hello " + list.get(0).getName(), body);
 
         }
         else
