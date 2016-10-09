16e51f31a655f9536af06195b45772f38b7b665c
RohanChhipa <rohan.chhipa@live.com>
2016-10-09 18:27:10 +0200

    Initialises forecast obejct

--- src/main/java/me/hyperperform/reporting/algorithm/StandardAlgorithm.java
+++ src/main/java/me/hyperperform/reporting/algorithm/StandardAlgorithm.java
@@ -11,6 +11,7 @@ import me.hyperperform.user.Position;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import javax.ejb.EJB;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -30,21 +31,8 @@ public class StandardAlgorithm implements Algorithm
 {
     private EntityManagerFactory entityManagerFactory;
     private EntityManager entityManager;
-
-    @Inject
+    
     IForecasting forecasting;
-
-    @PostConstruct
-    private void initConnection() {
-        entityManagerFactory = Persistence.createEntityManagerFactory("PostgreJPA");
-        entityManager = entityManagerFactory.createEntityManager();
-    }
-
-    @PreDestroy
-    private void disconnect() {
-        entityManager.close();
-        entityManagerFactory.close();
-    }
 
     /**
      * Implementation of the calculateScore found in {@see me.hyperperform.reporting.algorithm.Algorithm}.
@@ -56,92 +44,92 @@ public class StandardAlgorithm implements Algorithm
      */
     public CalculateScoreResponse calculateScore(CalculateScoreRequest calculateScoreRequest)
     {
+        entityManagerFactory = Persistence.createEntityManagerFactory("PostgreJPA");
+        entityManager = entityManagerFactory.createEntityManager();
+        forecasting = new Forecasting();
+
         /*-------------------Mapping Email to name----------------------*/
         Query q = entityManager.createQuery("SELECT a.gitUserName FROM User a WHERE userEmail=:email").setParameter("email", calculateScoreRequest.getName());
         String gitUserName = (String)q.getSingleResult();
         //*--------------------------------------------------------------*
 
-//        GetForecastTimeRequest getForecastTimeRequest = new GetForecastTimeRequest("GitCommits", getPosition(calculateScoreRequest.getName()));
-        System.out.println("======================================");
-        System.out.println(forecasting);
-        System.out.println("======================================");
+        long numOfDays = TimeUnit.DAYS.convert(calculateScoreRequest.getEndDate().getTime() - calculateScoreRequest.getStartDate().getTime(), TimeUnit.MILLISECONDS);
+        long time = TimeUnit.MILLISECONDS.toDays(calculateScoreRequest.getEndDate().getTime() - calculateScoreRequest.getStartDate().getTime());
 
-// long timeGit = convertDays(0, forecasting.getForecastTime(getForecastTimeRequest).getTime());
+        /*----------------------   GitHub   -----------------------------------*/
+        q = entityManager.createQuery("SELECT sum(a.commitSize) FROM GitPush a WHERE (timestamp BETWEEN :startDate AND :endDate) AND (username=:uname)").setParameter("startDate", calculateScoreRequest.getStartDate()).setParameter("endDate", calculateScoreRequest.getEndDate()).setParameter("uname", gitUserName);
 
-//        long numOfDays = TimeUnit.DAYS.convert(calculateScoreRequest.getEndDate().getTime() - calculateScoreRequest.getStartDate().getTime(), TimeUnit.MILLISECONDS);
-//        long time = TimeUnit.MILLISECONDS.toDays(calculateScoreRequest.getEndDate().getTime() - calculateScoreRequest.getStartDate().getTime());
-//
-//        /*----------------------   GitHub   -----------------------------------*/
-//        q = entityManager.createQuery("SELECT sum(a.commitSize) FROM GitPush a WHERE (timestamp BETWEEN :startDate AND :endDate) AND (username=:uname)").setParameter("startDate", calculateScoreRequest.getStartDate()).setParameter("endDate", calculateScoreRequest.getEndDate()).setParameter("uname", gitUserName);
-//
-//        Long tmp = (Long)q.getSingleResult();
-//        long totalCommits = (tmp == null) ? 0 : tmp;
-//
-//        GetForecastTimeRequest getForecastTimeRequest = new GetForecastTimeRequest("GitCommits", getPosition(calculateScoreRequest.getName()));
-//        long timeGit = convertDays(time, forecasting.getForecastTime(getForecastTimeRequest).getTime());
-//
-//        GetForecastValueRequest getForecastValueRequest = new GetForecastValueRequest("GitCommits", getPosition(calculateScoreRequest.getName()));
-//        double forecastValue = forecasting.getForecastValue(getForecastValueRequest).getValue();
-//
-//        double avg = (double) totalCommits / (double) timeGit;
+        Long tmp = (Long)q.getSingleResult();
+        long totalCommits = (tmp == null) ? 0 : tmp;
+
+        GetForecastTimeRequest getForecastTimeRequest = new GetForecastTimeRequest("GitCommits", getPosition(calculateScoreRequest.getName()));
+        long timeGit = convertDays(time, forecasting.getForecastTime(getForecastTimeRequest).getTime());
+
+        GetForecastValueRequest getForecastValueRequest = new GetForecastValueRequest("GitCommits", getPosition(calculateScoreRequest.getName()));
+        double forecastValue = forecasting.getForecastValue(getForecastValueRequest).getValue();
+
+        double avg = (double) totalCommits / (double) timeGit;
+        avg /= forecastValue;
+        tmp = (long) (avg * 10000.0);
+
+        double git = (double) (tmp) / 100.0;
+        System.out.println("\n\nGit: " + git );
+        /*---------------------------------------------------------------------*/
+
+
+
+        /*----------------------   Travis   -----------------------------------*/
+        q = entityManager.createQuery("SELECT COUNT(a.status) FROM TravisEvent a WHERE (timestamp BETWEEN :startDate AND :endDate) AND (commiter=:uname) AND (status LIKE 'Passed')").setParameter("startDate", calculateScoreRequest.getStartDate()).setParameter("endDate", calculateScoreRequest.getEndDate()).setParameter("uname", gitUserName);
+        tmp = (Long)q.getSingleResult();
+        long passed = (tmp == null) ? 0 : tmp;
+
+        q = entityManager.createQuery("SELECT COUNT(a.status) FROM TravisEvent a WHERE (timestamp BETWEEN :startDate AND :endDate) AND (commiter=:uname) AND (status LIKE 'Failed')").setParameter("startDate", calculateScoreRequest.getStartDate()).setParameter("endDate", calculateScoreRequest.getEndDate()).setParameter("uname", gitUserName);
+        tmp = (Long)q.getSingleResult();
+        long failed = (tmp == null) ? 1 : tmp;
+
+
+        getForecastTimeRequest = new GetForecastTimeRequest("TravisBuild", getPosition(calculateScoreRequest.getName()));
+        long timeTravis = convertDays(time, forecasting.getForecastTime(getForecastTimeRequest).getTime());
+
+        getForecastValueRequest = new GetForecastValueRequest("TravisBuild", getPosition(calculateScoreRequest.getName()));
+        forecastValue = forecasting.getForecastValue(getForecastValueRequest).getValue();
+
+        avg = (double) passed / (double)(passed+failed);
+        avg *= 100.0;
+
+        double travis = avg - forecastValue;
+//        avg = (double) passed / (double) timeTravis;
 //        avg /= forecastValue;
 //        tmp = (long) (avg * 10000.0);
 //
-//        double git = (double) (tmp) / 100.0;
-//        System.out.println("\n\nGit: " + git );
-//        /*---------------------------------------------------------------------*/
-//
-//
-//
-//        /*----------------------   Travis   -----------------------------------*/
-//        q = entityManager.createQuery("SELECT COUNT(a.status) FROM TravisEvent a WHERE (timestamp BETWEEN :startDate AND :endDate) AND (commiter=:uname) AND (status LIKE 'Passed')").setParameter("startDate", calculateScoreRequest.getStartDate()).setParameter("endDate", calculateScoreRequest.getEndDate()).setParameter("uname", gitUserName);
-//        tmp = (Long)q.getSingleResult();
-//        long passed = (tmp == null) ? 0 : tmp;
-//
-//        q = entityManager.createQuery("SELECT COUNT(a.status) FROM TravisEvent a WHERE (timestamp BETWEEN :startDate AND :endDate) AND (commiter=:uname) AND (status LIKE 'Failed')").setParameter("startDate", calculateScoreRequest.getStartDate()).setParameter("endDate", calculateScoreRequest.getEndDate()).setParameter("uname", gitUserName);
-//        tmp = (Long)q.getSingleResult();
-//        long failed = (tmp == null) ? 1 : tmp;
-//
-//
-//        getForecastTimeRequest = new GetForecastTimeRequest("TravisBuild", getPosition(calculateScoreRequest.getName()));
-//        long timeTravis = convertDays(time, forecasting.getForecastTime(getForecastTimeRequest).getTime());
-//
-//        getForecastValueRequest = new GetForecastValueRequest("TravisBuild", getPosition(calculateScoreRequest.getName()));
-//        forecastValue = forecasting.getForecastValue(getForecastValueRequest).getValue();
-//
-//        avg = (double) passed / (double)(passed+failed);
-//        avg *= 100.0;
-//
-//        double travis = avg - forecastValue;
-////        avg = (double) passed / (double) timeTravis;
-////        avg /= forecastValue;
-////        tmp = (long) (avg * 10000.0);
-////
-////        double travis = (double) (tmp) / 100.0;
-//        System.out.println("\n\nTravis: " + travis );
-//        /*---------------------------------------------------------------------*/
-//
-//
-//        /*----------------------   Issues   -----------------------------------*/
-//        double issues = 0.0;
-//        /*---------------------------------------------------------------------*/
-//
-//        /*----------------------   Entry   -----------------------------------*/
-//         double entry = 0.0;
-//        /*---------------------------------------------------------------------*/
-//
-//
-//
-//        /*---------------------------------------------------------------------*/
-//        /*-------------------      Score  Generation    -----------------------*/
-//        /*---------------------------------------------------------------------*/
-//        double score = (0.4*(git)) + (0.2*(travis) + (0.2*(issues)) + (0.2*(entry)));
-//
-//        score = scale(score, 0.0, 5.0);
-//
-//        return new CalculateScoreResponse((Double.isNaN(score)) ? 0.0 : score);
+//        double travis = (double) (tmp) / 100.0;
+        System.out.println("\n\nTravis: " + travis );
+        /*---------------------------------------------------------------------*/
 
-        return new CalculateScoreResponse(0.0);
+
+        /*----------------------   Issues   -----------------------------------*/
+        double issues = 0.0;
+        /*---------------------------------------------------------------------*/
+
+        /*----------------------   Entry   -----------------------------------*/
+         double entry = 0.0;
+        /*---------------------------------------------------------------------*/
+
+
+
+        /*---------------------------------------------------------------------*/
+        /*-------------------      Score  Generation    -----------------------*/
+        /*---------------------------------------------------------------------*/
+        double score = (0.4*(git)) + (0.2*(travis) + (0.2*(issues)) + (0.2*(entry)));
+
+        score = scale(score, 0.0, 5.0);
+
+        entityManager.close();
+        entityManagerFactory.close();
+
+        return new CalculateScoreResponse((Double.isNaN(score)) ? 0.0 : score);
+
+//        return new CalculateScoreResponse(0.0);
     }
 
 
