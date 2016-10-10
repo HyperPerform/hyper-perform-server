d53e66c21ac7079ef587218077d17aeca7d80777
JasonDGordon <u14405025@tuks.co.za>
2016-10-10 20:40:31 +0200

    Changed the created by and added descriptions for javadoc

--- src/main/java/me/hyperperform/forecasting/response/DeleteIntegrationResponse.java
+++ src/main/java/me/hyperperform/forecasting/response/DeleteIntegrationResponse.java
@@ -1,7 +1,11 @@
 package me.hyperperform.forecasting.response;
 
 /**
- * Created by rohan on 2016/10/02.
+ * A POJO for the response to be sent back to the caller after the respective request (@see me.hyperperform.forecasting.request.DeleteIntegrationRequest) and deletion from the database has been made.
+ *
+ * @author  CodusMaximus
+ * @version 1.0
+ * @since   2016/10/01
  */
 public class DeleteIntegrationResponse
 {
--- src/main/java/me/hyperperform/forecasting/response/UpdateIntegrationResponse.java
+++ src/main/java/me/hyperperform/forecasting/response/UpdateIntegrationResponse.java
@@ -1,5 +1,13 @@
 package me.hyperperform.forecasting.response;
 
+/**
+ * A POJO for the response to be sent back to the caller after the respective request (@see me.hyperperform.forecasting.request.UpdateIntegrationRequest) and update of the database has been made.
+ *
+ * @author  CodusMaximus
+ * @version 1.0
+ * @since   2016/09/30
+ */
+
 public class UpdateIntegrationResponse
 {
     private boolean updated;
