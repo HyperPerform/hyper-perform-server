ab25f3e117d2cb4b2930acf449ad4a4a21114c3f
AvinashSingh786 <u14043778@tuks.co.za>
2016-10-10 21:43:16 +0200

    added the package info for User  sub  modules

---
+++ src/main/java/me/hyperperform/user/request/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Requests for all user related management operations for encapsulation
+ */
+package me.hyperperform.user.request;
---
+++ src/main/java/me/hyperperform/user/response/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Responses for all user related management operations for encapsulation
+ */
+package me.hyperperform.user.response;
