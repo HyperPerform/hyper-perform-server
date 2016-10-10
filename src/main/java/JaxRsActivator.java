02482797eec6ad4f9a67caf5ea3ebffc47c8f237
AvinashSingh786 <u14043778@tuks.co.za>
2016-10-10 21:00:04 +0200

    javadoc the jax activator

--- src/main/java/JaxRsActivator.java
+++ src/main/java/JaxRsActivator.java
@@ -1,6 +1,14 @@
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;  
-  
+import javax.ws.rs.core.Application;
+
+/**
+ * The root path that activates the rest, configures the JaxRS restful services with an RESTeasy implementation
+ *
+ * @author  CodusMaximus
+ * @version 1.0
+ * @since   2016
+ */
+
 @ApplicationPath("/rs")
 public class JaxRsActivator extends Application { 
 	public JaxRsActivator()
