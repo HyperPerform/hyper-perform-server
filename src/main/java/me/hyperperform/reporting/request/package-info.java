606c91892658e1c6cd0e0228a21d52fccd67c5d4
AvinashSingh786 <u14043778@tuks.co.za>
2016-10-10 21:44:55 +0200

    added the package info for reporting  sub  modules

---
+++ src/main/java/me/hyperperform/reporting/request/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Requests for all reporting operations for encapsulation
+ */
+package me.hyperperform.reporting.request;
---
+++ src/main/java/me/hyperperform/reporting/response/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Response for all reporting operations for encapsulation
+ */
+package me.hyperperform.reporting.response;
