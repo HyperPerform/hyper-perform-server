6b1974d75b7b3d34cb9c2809b5327e2d46968ed8
AvinashSingh786 <u14043778@tuks.co.za>
2016-10-10 21:01:48 +0200

    explained the enums

--- src/main/java/me/hyperperform/user/Position.java
+++ src/main/java/me/hyperperform/user/Position.java
@@ -1,16 +1,21 @@
 package me.hyperperform.user;
 
 /**
- * hyperperform-system
- * Group: CodusMaximus
- * Date: 2016/09/09
- * Feature:
+ * Defines position/job descriptions for various employees in the business
+ *
+ * @author  CodusMaximus
+ * @version 1.0
+ * @since   2016/09/09
  */
 public enum Position
 {
     WebDeveloper("Web Developer"),
     SoftwareDeveloper("Software Developer"),
     Multimedia("Multimedia"),
+    /**
+     * Has more permissions than other users, also allows criteria not to allow PA score generation
+     * as implemented in the strategy in the report generator
+     */
     Manager("Manager");
 
     private String type;
