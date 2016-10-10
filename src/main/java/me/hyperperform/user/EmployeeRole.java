3d6a773a1b6f584149510c5d0efeb56626d67836
AvinashSingh786 <u14043778@tuks.co.za>
2016-10-10 21:02:19 +0200

    added javadoc and the permissions that each role has in the system

--- src/main/java/me/hyperperform/user/EmployeeRole.java
+++ src/main/java/me/hyperperform/user/EmployeeRole.java
@@ -1,15 +1,27 @@
 package me.hyperperform.user;
 
 /**
- * hyperperform-system
- * Group: CodusMaximus
- * Date: 2016/09/09
- * Feature:
+ * This Emum defines the level of permission as roles to access certain parts of the hyperperform system
+ *
+ * @author  CodusMaximus
+ * @version 1.0
+ * @since   2016/09/09
  */
 public enum EmployeeRole
 {
+    /**
+     * Can do anything in the system has the highest amount of privileges
+     */
     Super("Super"),
+
+    /**
+     * Can add more employees and view multiple employees profiles
+     */
     Administrator("Administrator"),
+
+    /**
+     * Can only view information about themselves have the least amount of privileges
+     */
     Employee("Employee");
 
 
