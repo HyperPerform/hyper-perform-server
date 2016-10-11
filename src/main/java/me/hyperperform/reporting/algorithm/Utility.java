4c20918fb76b46662597fe470549aa424ed8aef0
RohanChhipa <rohan.chhipa@live.com>
2016-10-11 11:27:17 +0200

    added convertDays and getPosition functions

--- src/main/java/me/hyperperform/reporting/algorithm/Utility.java
+++ src/main/java/me/hyperperform/reporting/algorithm/Utility.java
@@ -1,5 +1,9 @@
 package me.hyperperform.reporting.algorithm;
 
+import me.hyperperform.user.Position;
+
+import javax.persistence.Query;
+
 /**
  * Created by rohan on 2016/10/11.
  */
@@ -11,5 +15,30 @@ public class Utility
         value = (value*(end-start)) + start;
 
         return (value > end) ? end : value;
+    }
+
+    public String getPosition(String user) {
+        Query q = entityManager.createQuery("select position from User where userEmail=:email").setParameter("email", user);
+        Position p = (Position) q.getSingleResult();
+
+        return (p == null) ? null : p.getType();
+    }
+
+    public long convertDays(long days, String time)
+    {
+        System.out.println("D: " + days + "  " + time);
+        if (time.equals("week"))
+        {
+            if (days != 0)
+                return days/7;
+        }
+
+        if (time.equals("month"))
+        {
+            if (days != 0)
+                return days/30;
+        }
+
+        return days;
     }
 }
