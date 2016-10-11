0d14eb85c19ef6ac4d2ad9f5c3d0d81fd2f18b7b
AvinashSingh786 <u14043778@tuks.co.za>
2016-10-11 17:56:54 +0200

    added mock event for no password

--- src/test/java/me/hyperperform/user/MockUsers.java
+++ src/test/java/me/hyperperform/user/MockUsers.java
@@ -10,7 +10,9 @@ public class MockUsers
 
     static String noEmail = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\", \"gitUserName\": \"rohan\"}";
 
-    static String noGitUsername = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"Outside\", \"gitUserName\": \"\"}";
+    static String noPassword = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\" \", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\", \"gitUserName\": \"rohan\"}";
+
+    static String noGitUsername = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Employee\", \"position\":\"SoftwareDeveloper\", \"gitUserName\": \"\"}";
 
     static String invalidRole = "{\"managerEmail\": \"admin@hyperperform.me\", \"userName\":\"rohan\", \"userSurname\":\"chhipa\", \"userEmail\":\"rohanchhipa@live.com\", \"userPassword\":\"1234\", \"role\":\"Jester\", \"position\":\"SoftwareDeveloper\", \"gitUserName\": \"rohan\"}";
 
